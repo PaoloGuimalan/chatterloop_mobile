6f122d6bfca87dddcaf4c0321fa6c8cbba7423dc
Paolo <pauloportes.guimalan187@gmail.com>
2024-12-30 19:47:42 +0800

    feat: contacts list and item layouts has been initialized but no functionalities yet

--- lib/views/home/tabs/contacts_view.dart
+++ lib/views/home/tabs/contacts_view.dart
@@ -1,9 +1,12 @@
 import 'package:chatterloop_app/core/configs/keys.dart';
+import 'package:chatterloop_app/core/redux/state.dart';
 import 'package:chatterloop_app/core/requests/http_requests.dart';
+import 'package:chatterloop_app/core/reusables/widgets/contacts_item.dart';
 import 'package:chatterloop_app/models/http_models/response_models.dart';
 import 'package:chatterloop_app/models/user_models/user_contacts_model.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 class ContactsView extends StatefulWidget {
   const ContactsView({super.key});
@@ -46,52 +49,72 @@ class ContactsStateView extends State<ContactsView> {
 
   @override
   Widget build(BuildContext context) {
-    if (contactsList.isEmpty) {
-      getContactsProcess();
-    }
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-            child: Padding(
-          padding: EdgeInsets.only(top: 5, left: 5, right: 5),
-          child: Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: 50,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.contact_page_sharp,
-                      size: 30,
-                      color: Color(0xffff7043),
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text("Contacts",
-                        style: TextStyle(
-                            fontSize: 17,
-                            color: Color(0xFF565656),
-                            fontWeight: FontWeight.bold)),
-                  ],
+    return StoreConnector<AppState, AppState>(builder: (context, state) {
+      if (contactsList.isEmpty) {
+        getContactsProcess();
+      }
+      return MaterialApp(
+        home: Scaffold(
+          body: Center(
+              child: Padding(
+            padding: EdgeInsets.only(top: 5, left: 5, right: 5),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.contact_page_sharp,
+                        size: 30,
+                        color: Color(0xffff7043),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text("Contacts",
+                          style: TextStyle(
+                              fontSize: 17,
+                              color: Color(0xFF565656),
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(
-                child: ListView.builder(
-                    padding: EdgeInsets.only(top: 0, bottom: 10),
-                    shrinkWrap: true,
-                    // controller: _scrollController,
-                    itemCount: contactsList.length,
-                    itemBuilder: (context, index) {
-                      return Text(contactsList[index].contactID);
-                    }),
-              )
-            ],
-          ),
-        )),
-      ),
-    );
+                Expanded(
+                  child: ListView.builder(
+                      padding: EdgeInsets.only(
+                          top: 0, bottom: 10, left: 10, right: 10),
+                      shrinkWrap: true,
+                      // controller: _scrollController,
+                      itemCount: contactsList.length,
+                      itemBuilder: (context, index) {
+                        if (contactsList[index].type == "single") {
+                          if (contactsList[index].userdetails.userone.userID ==
+                              state.userAuth.user.userID) {
+                            return ContactsItemWidget(
+                                contact:
+                                    contactsList[index].userdetails.usertwo!);
+                          } else {
+                            return ContactsItemWidget(
+                                contact:
+                                    contactsList[index].userdetails.userone);
+                          }
+                        }
+
+                        return SizedBox(
+                          height: 0,
+                        );
+                      }),
+                )
+              ],
+            ),
+          )),
+        ),
+      );
+    }, converter: (store) {
+      return store.state;
+    });
   }
 }
